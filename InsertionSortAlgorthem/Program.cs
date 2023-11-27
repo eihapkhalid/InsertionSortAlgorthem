8d7d4571dbc62cd3ee5a9a91bef51ae76b5c4cb0
eihapKhalid <eihapinfo@gmail.com>
2023-11-27 17:01:59 +0300

    update program with servies

--- InsertionSortAlgorthem/Program.cs
+++ InsertionSortAlgorthem/Program.cs
@@ -1,4 +1,11 @@
 
+using InsertionSortAlgorthem.DataAccess.Data;
+using InsertionSortAlgorthem.DataAccess.UnitOfWork;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 namespace InsertionSortAlgorthem
 {
     public class Program
@@ -11,8 +18,45 @@ namespace InsertionSortAlgorthem
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            #region AddEndpointsApiExplorer
             builder.Services.AddEndpointsApiExplorer();
+            #endregion
+
+            #region AddSwaggerGen
             builder.Services.AddSwaggerGen();
+            #endregion
+
+            #region JwtBearer Authentication
+            // Install-Package Microsoft.AspNetCore.Authentication.JwtBearer
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey
+                    (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = false,
+                    ValidateIssuerSigningKey = true
+                };
+            });
+            #endregion
+
+            #region DefaultConnection
+            builder.Services.AddDbContext<SortAlgoDbContext>(options =>
+                        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            #endregion
+
+            #region UnitOfWork
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            #endregion
 
             var app = builder.Build();
 
