f72d591163972dbf75b09c6372479ff55204fbd3
eihapKhalid <eihapinfo@gmail.com>
2023-11-28 11:54:46 +0300

    create migration

---
+++ InsertionSortAlgorthem.DataAccess/Migrations/20231128085357_FirstMigration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InsertionSortAlgorthem.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TbEmbloyees",
+                columns: table => new
+                {
+                    EmbloyeeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmbloyeeName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    EmbloyeeAge = table.Column<int>(type: "int", nullable: false),
+                    EmbloyeeSalary = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    EmbloyeeIsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TbEmbloyees", x => x.EmbloyeeId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TbEmbloyees");
+        }
+    }
+}
---
+++ InsertionSortAlgorthem/Controllers/EmployeeController.cs
@@ -0,0 +1,61 @@
+﻿using InsertionSortAlgorthem.DataAccess.UnitOfWork;
+using InsertionSortAlgorthem.Models;
+using InsertionSortAlgorthem.Utilities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsertionSortAlgorthem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        #region Inject
+        private readonly IUnitOfWork _unitOfWork;
+        private InsertionSortingService _insertionSortingService;
+
+        public EmployeeController(IUnitOfWork unitOfWork, InsertionSortingService insertionSortingService)
+        {
+            _unitOfWork = unitOfWork;
+            _insertionSortingService = insertionSortingService;
+        }
+        #endregion
+
+        [HttpPost("PostEmployee")]
+        public IActionResult PostEmployee([FromBody] TbEmbloyee model)
+        {
+            try
+            {
+                var foundEmployee = _unitOfWork.TbEmbloyeeRepository.Get(e=>e.EmbloyeeId == model.EmbloyeeId);
+                if (foundEmployee != null)
+                {
+                    #region Update
+                    // تحديث الإعدادات الحالية
+                    foundEmployee.EmbloyeeId = model.EmbloyeeId;
+                    foundEmployee.EmbloyeeName = model.EmbloyeeName;
+                    foundEmployee.EmbloyeeAge = model.EmbloyeeAge;
+                    foundEmployee.EmbloyeeIsActive = model.EmbloyeeIsActive;
+                    foundEmployee.EmbloyeeSalary = model.EmbloyeeSalary;
+                    _unitOfWork.Save(); // لحفظ التغييرات في قاعدة البيانات
+                    #endregion
+                }
+                else
+                {
+                    #region Add new
+                    // إضافة موظف جديد
+                    _unitOfWork.TbEmbloyeeRepository.Add(model);
+                    _unitOfWork.Save(); // لحفظ التغييرات في قاعدة البيانات 
+                    #endregion
+                }
+
+                return Ok(model); // إرجاع النموذج بالبيانات المحدثة أو الجديدة
+            }
+            catch (Exception ex)
+            {
+                // التعامل مع الاستثناءات
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+    }
+}
